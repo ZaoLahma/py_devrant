f9bd1a051cc4dfb350d0f59e4cc60c30aaf075f2
Janne <thyberg.jan@gmail.com>
2016-06-08 19:29:51 +0200

    Cleanup of gui

--- gui.py
+++ gui.py
@@ -4,15 +4,15 @@ class GUI:
         if "MAIN_MENU" == state:
             self.print_logo()
             print("This is a completely unofficial Devrant (www.devrant.io) reader.")
-            print("====== MENU ======")
-            print("view - View most recent rants")
+            print("== COMMANDS ===========================================================================")
+            print("view               - View most recent rants")
             print("view <no_of_rants> - View most recent rants, printing a maximum of 50 rants")            
-            print("top <no_of_rants> - View the top rated rants, returning at most 50 rants")
-            print("surprise - Get a random rant")
-            print("search <term> - Search for rant containing <term> returning a maximum of 20 rants")
-            print("get <rant_id> - Get a specific rant and its comments")
-            print("r - Refresh/Repeat previous command")
-            print("exit - Exit program")
+            print("top <no_of_rants>  - View the top rated rants, returning at most 50 rants")
+            print("surprise           - Get a random rant")
+            print("search <term>      - Search for rant containing <term> returning a maximum of 20 rants")
+            print("get <rant_id>      - Get a specific rant and its comments")
+            print("r                  - Refresh/Repeat previous command")
+            print("exit               - Exit program")
             user_input = input("Command: ")
             
         return user_input

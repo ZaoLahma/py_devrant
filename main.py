119c88fdb7f9ac91b186b2448e60aa7dc5a2f7f7
Janne <thyberg.jan@gmail.com>
2016-06-17 21:57:48 +0200

    Fixed project file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 from input_handler import InputHandler
 if __name__ == "__main__":
-    #devrant_manager = DevRantManager()
-    #devrant_manager.start()
     input_handler = InputHandler()
     input_handler.handle_input()
